88e6021b9d58b4a0911576154b4041e913d38ef6
Peter Omollo <omollpeter@gmail.com>
2023-10-05 19:52:54 +0300

    Create a C program that prints a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts(str) - Prints str
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
