067cc2b8db31971c41cf0d5568245fe1d56abf36
Peter Omollo <omollpeter@gmail.com>
2023-10-06 18:19:58 +0300

    Create a program to print all single digits with a comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Program prints all digits with a comma followed by space
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	i = 58;
+
+	for (n = 48; n < i; n++)
+	{
+		putchar(n);
+		putchar((n == 57) ? ' ' : ',');
+		putchar((n == 57) ? '\n' : ' ');
+	}
+
+	return (0);
+}
