27759355cb465ee28ec5dd4c18f42312e7e8c3ed
Peter Omollo <omollpeter@gmail.com>
2023-10-06 16:24:38 +0300

    Create a script that prints alphabets but omits some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Program prints all lowercase alphabet characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	i = 123;
+
+	while (c < i)
+	{
+		putchar((c == 101 || c == 113) ? '\0' : c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
