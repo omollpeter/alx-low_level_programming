580d27beaedf6842ff4f62fd5b7f95bfb1b6d461
Peter Omollo <omollpeter@gmail.com>
2023-10-11 13:41:02 +0300

    Create a program that checks if a character is a letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
-/**
  * _isalpha - Checks if a charcter is a letter
  * @c: Character to check
  *
@@ -36,16 +15,4 @@ int _isalpha(int c)
 		return (1);
 	else
 		return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
