1fff4206e56bd192606cdcb013262d3f1fb6934c
Peter Omollo <omollpeter@gmail.com>
2023-10-12 01:38:22 +0300

    Create a program that computes and prints sum of all multiples of 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
