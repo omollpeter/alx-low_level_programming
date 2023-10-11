5bb7452bcb410f267f1fa0132aff280a001a7f9d
Peter Omollo <omollpeter@gmail.com>
2023-10-12 01:07:09 +0300

    Create a program that prints different level of times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,11 +8,13 @@
  */
 void print_times_table(int n)
 {
+	int i, j;
+
 	if (n < 0 || n > 15)
 		return;
-	for (int i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			int x = i * j;
 
