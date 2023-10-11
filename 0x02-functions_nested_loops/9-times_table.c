da7f86df13abf3bec051c091962fdff8e0886053
Peter Omollo <omollpeter@gmail.com>
2023-10-11 21:35:08 +0300

    Create a program that prints times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void times_table(void)
+{
+    int i;
+    int j;
+    int x;
+
+    for (i = 0; i < 10; i++)
+    {   
+        for (j = 0; j < 10; j++)
+        {
+            x = i * j;
+            if (x < 10) 
+            {
+                _putchar(x % 10 + '0');
+                if (j == 9)
+                    continue;
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+            }
+            else if (x >= 10) 
+            {
+                _putchar(x / 10 + '0');
+                _putchar(x % 10 + '0');
+                if (j == 9)
+                    continue;
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }   
+}
