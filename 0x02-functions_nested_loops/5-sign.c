912e3d0e86638f8e3508417575f07297aa73b8bf
Peter Omollo <omollpeter@gmail.com>
2023-10-11 16:27:31 +0300

    Create a program that prints sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks an integer and prints +. -, or 0
+ * @n: Integer to be checked
+ *
+ * Return: 1 if positive, 0 if equals 0 or -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
