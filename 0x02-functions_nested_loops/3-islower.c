ab175704ee94267535fecc672b74bd429d7875af
Peter Omollo <omollpeter@gmail.com>
2023-10-11 13:37:56 +0300

    Create a program that checks if a character is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
-/**
  * _islower - Checks if a charcter is lowercase
  * @c: Character to check
  *
@@ -32,16 +13,4 @@ int _islower(int c)
 		return (1);
 	else
 		return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
