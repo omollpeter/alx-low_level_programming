46afd732689624acd83ef7453da0a6bdf2a29425
Peter Omollo <omollpeter@gmail.com>
2023-10-11 17:43:13 +0300

    Create a program that prints and return the last digit of an integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,11 +15,15 @@ int print_last_digit(int n)
 		ld = n % 10;
 		_putchar(ld + '0');
 	}
-	else if (n < 0)
+	else if (n < 0 && n > INT_MIN)
 	{
 		ld = (n / -1) % 10;
 		_putchar(ld + '0');
 	}
+	else if (n == INT_MIN)
+	{
+		n = (n / -1) / 10;
+		ld = n % 10;
 
 	return (ld);
 }
