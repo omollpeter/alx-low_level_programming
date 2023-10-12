41457d4251748f566036346df9c1c5c6bd864865
Peter Omollo <omollpeter@gmail.com>
2023-10-12 06:43:27 +0300

    Create a program that prints the first 98 numbers of fibonacci sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 2;
+
+	for (; i < 100; i++)
+	{
+		printf("%d", fibonacci(i));
+		if (i == 99)
+			continue;
+		printf(", ");
+	}
+	putchar('\n');
+}
+
+/**
+ * fibonacci - Prints fibonacci number specified
+ * @n: Integer specifying fibonacci position to print
+ *
+ * Return: Integer
+ */
+int fibonacci(int n)
+{
+	if (n < 2)
+		return (n);
+	else
+		return (fibonacci(n - 1) + fibonacci(n - 2));
+}
