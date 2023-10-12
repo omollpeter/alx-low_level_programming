6debdd24cbc84461e42154afa083ed9c83fa2919
Peter Omollo <omollpeter@gmail.com>
2023-10-13 01:24:13 +0300

    Create a a function that prints integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,7 +17,7 @@ void print_number(int n)
 	if (n >= 0 && n < 10)
 		_putchar(n % 10 + '0');
 
-	else if (n >= 10)
+	else if (n >= 10 && n < 100)
 	{
 		_putchar((n % 100) / 10  + '0');
 		_putchar(n % 10 + '0');
