7bd761805b129121997d64002133d150a7748fd1
Peter Omollo <omollpeter@gmail.com>
2023-10-12 09:36:00 +0300

    Create a program that checks if a character is uppercase

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#### This archive contains projects about more functions and nested loops.
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+	#include <unistd.h>
+	#include <limits.h>
+	#include <stdio.h>
+
+	/* Function prototypes */
+	int _putchar(char c);
+	int _isupper(int c);
+
+#endif
