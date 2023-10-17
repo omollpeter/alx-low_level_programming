ceec270f336496cc78c819f81fd55402039d41f9
Peter Omollo <omollpeter@gmail.com>
2023-10-17 23:45:31 +0300

    Create a program that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random password
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	srand(time(NULL));
+
+	rand();
+
+	return (0);
+}
