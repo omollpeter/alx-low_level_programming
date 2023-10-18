8d967f9d2294b5d466b30eb54a4033c242edd041
Peter Omollo <omollpeter@gmail.com>
2023-10-18 05:50:13 +0300

    Create a program that generates random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,15 +2,32 @@
 #include <time.h>
 
 /**
- * main - Generates random password
+ * main - Generates random password for 101-crackme
  *
  * Return: 0 Always (Success)
  */
 int main(void)
 {
+	int password[100];
+	int i;
+	int sum = 0;
+	int n;
+
 	srand(time(NULL));
 
-	rand();
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 78;
+		sum += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
 	return (0);
 }
