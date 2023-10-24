c503e966c70b24edcd9bf02d72a41d7549e98d1a
Peter Omollo <omollpeter@gmail.com>
2023-10-24 05:48:28 +0300

    Create a function that locates bytes in a string and return ptr to one of bytes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -17,5 +17,6 @@
 	void set_string(char **s, char *to);
 	void print_diagsums(int *a, int size);
 	char *_strstr(char *haystack, char *needle);
+	char *_strpbrk(char *s, char *accept);
 
 #endif /* MAIN_H */
