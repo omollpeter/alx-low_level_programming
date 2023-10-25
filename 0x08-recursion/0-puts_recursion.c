041d06ee02c23b2165ea033af14595f8b213cd7a
Peter Omollo <omollpeter@gmail.com>
2023-10-25 12:48:10 +0300

    Create a function that prints a string using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string using recursion
+ * @s: String to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 1;
+
+	if (strlen(s) == 0)
+		return;
+	_putchar(s[0]);
+
+	if (s[0 + i])
+	{
+		_puts_recursion(s + i);
+		i++;
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+	#include <stdio.h>
+	#include <unistd.h>
+	#include <string.h>
+	#include <limits.h>
+	#include <stddef.h>
+
+	/* Function prototypes */
+	int _putchar(char c);
+	void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
