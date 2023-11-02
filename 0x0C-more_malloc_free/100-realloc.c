8bcc6f1a74fa9f6ec6d97fa3a0aeaa1e1efb74cb
Peter Omollo <omollpeter@gmail.com>
2023-11-02 21:54:01 +0300

    Fix 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,6 +10,9 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *new_ptr;
+	unsigned int copy_size;
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -18,18 +21,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		ptr = malloc(new_size);
+		new_ptr = malloc(new_size);
 	}
 
-	if (new_size > old_size)
+	if (new_size > old_size || new_size < old_size)
 	{
-		ptr = malloc(new_size);
+		new_ptr = malloc(new_size);
 	}
 
-	if (new_size < old_size)
-	{
-		ptr = malloc(new_size);
-	}
+	copy_size = (new_size < old_size) ? new_size : old_size;
+	memcpy(new_ptr, ptr, copy_size);
 
-	return (ptr);
+	return (new_ptr);
 }
