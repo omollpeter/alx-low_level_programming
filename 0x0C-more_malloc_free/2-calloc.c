3bdd1ca7b6e3c61eebe478ada845a026e8b54a79
Peter Omollo <omollpeter@gmail.com>
2023-11-02 20:40:37 +0300

    Fix 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,8 +9,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
-	unsigned int i;
+	void *ptr;
 
 	if (!(nmemb || size))
 		return (NULL);
@@ -18,8 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
+	memset(ptr, 0, nmemb);
 
 	return (ptr);
 }
