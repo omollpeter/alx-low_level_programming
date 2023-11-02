381ef70b3edab972fc7dd196384c45e56f6845de
Peter Omollo <omollpeter@gmail.com>
2023-11-02 21:37:06 +0300

    Create a function that implements realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _realloc - Resizes a previously allocated memory block
+ * @ptr: Previously allocated memory block
+ * @old_size: Size in bytes of the prev allocated block
+ * @new_size: New size to allocate to ptr
+ *
+ * Return: Pointer to the new resized block, NULL otherwise
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
+
+	if (new_size > old_size)
+	{
+		ptr = malloc(new_size);
+	}
+
+	if (new_size < old_size)
+	{
+		ptr = malloc(new_size);
+	}
+
+	return (ptr);
+}
