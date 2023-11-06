0743ee679d1249e9316ac0b95a1cd03af4abc5ca
Peter Omollo <omollpeter@gmail.com>
2023-11-06 11:34:27 +0300

    Create a function that frees memory allocated to structure data type

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated to a variable of type struct dog
+ * @d: Variable to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
