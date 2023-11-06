49f6947cb9225b052b865ca696f24d4117267f02
Peter Omollo <omollpeter@gmail.com>
2023-11-06 10:15:24 +0300

    Create a header file that contains structure declaration for a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Declaration contains elems of a dog like name, age & owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
