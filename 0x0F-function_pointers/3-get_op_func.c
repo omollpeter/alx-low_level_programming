53c1f4f27067316ba3aa1c12a3bea2bff292a8b5
Peter Omollo <omollpeter@gmail.com>
2023-11-08 22:51:38 +0300

    Fix function that performs mathematical operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL && ops[i].f != NULL)
+	while ((ops[i].op != NULL && ops[i].f != NULL) || s == NULL)
 	{
 		if (s[0] == ops[i].op[0])
 		{
