6f0a99b263160ee057b7c519d6ea3d91bc75b534
Peter Omollo <omollpeter@gmail.com>
2023-11-08 22:46:43 +0300

    Create a program that performs simple mathematical operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Searches for the correct operation function depending on
+ *				the operator provided
+ * @s: The operator supplied in the command line
+ *
+ * Return: pointer to the required operation function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL && ops[i].f != NULL)
+	{
+		if (s[0] == ops[i].op[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of the two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Difference between the two integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Product of the two integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Quotient of the two integers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Computes remainder from division of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
