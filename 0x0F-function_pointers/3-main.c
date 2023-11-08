be762b04bce0b6b60e3592f9e35afbbfb9043a69
Peter Omollo <omollpeter@gmail.com>
2023-11-08 22:48:03 +0300

    Create a program that performs simple mathematical operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - Performs simple mathematical opeartions and prints result
+ * @argc: Number of program parameters supplied
+ * @argv: Program parameters
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+	int (*ptrop)(int, int);
+
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	ptrop = get_op_func(argv[2]);
+	if (ptrop == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((ptrop == op_div || ptrop == op_mod) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = ptrop(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
