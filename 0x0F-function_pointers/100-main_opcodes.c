7c7813b621e8335a319a5c0534e159a1f4a05ec1
Peter Omollo <omollpeter@gmail.com>
2023-11-09 00:39:21 +0300

    Create a progrm that prints its opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the opcode of a code
+ * @argc: No of programm parameters
+ * @argv: Program parameters
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+
+	if (argc < 2 || argc > 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num1 = atoi(argv[1]);
+	if (name < 0)
+	{
+		printd("Errow\n");
+		exit(2);
+	}
+	return (0);
+}
