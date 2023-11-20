5ff64e9eea1146459b59b655e547107fc2acd39a
Peter Omollo <omollpeter@gmail.com>
2023-11-20 11:16:54 +0300

    Create a function that frees memory allocated to linked list, sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees memory allocated to linked list and sets head to NULL
+ * @head: Pointer to pointer to the first node in the linked list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
