3b85ca5313446571c58af24dfe6cc318aa56b482
Peter Omollo <omollpeter@gmail.com>
2023-11-23 15:27:37 +0300

    Fix 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,24 +9,59 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i = 0;
-	char bits[33];
+	unsigned long num;
+	unsigned int j = 0;
+	int bits[32], i = 0;
 
 	if (n == 0)
 	{
-		bits[i] = 0;
+		bits[j] = 0;
 	}
 	else
 	{
-		while (n > 0)
+		while (1)
 		{
-			bits[i] = n % 2 + '0';
-			n /= 2;
+			num = calc_power(2, (unsigned long) i);
+			if (num == n)
+				break;
+			if (num > n)
+			{
+				i = i - 1;
+				break;
+			}
 			i++;
 		}
-		bits[i] = '\0';
+		while (i >= 0)
+		{
+			if (calc_power(2, (unsigned long) i) <= n)
+			{
+				bits[j] = 1;
+				n -= calc_power(2, (unsigned long) i);
+			}
+			else
+				bits[j] = 0;
+			i--;
+			j++;
+		}
 	}
-	if (index > i - 1)
+
+	j--;
+	if (index > j)
 		return (-1);
-	return (bits[index] - '0');
+	return (bits[j - index]);
+}
+
+/**
+ * calc_power - Computes power problems
+ * @base: Base number
+ * @power: Index/power
+ *
+ * Return: Computed value
+ */
+unsigned long calc_power(unsigned long base, unsigned long power)
+{
+	if (power == 0)
+		return (1);
+	else
+		return (base * calc_power(base, power - 1));
 }
