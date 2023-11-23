aee6af79968dbe2fdf6000cfe01571de8cf7c6d6
Peter Omollo <omollpeter@gmail.com>
2023-11-23 14:49:24 +0300

    Fix function that retrieves bit from a binary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,57 +9,24 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long num;
-	unsigned int j = 0;
-	int bits[32], i = 0;
+	unsigned int i = 0;
+	char bits[33];
 
 	if (n == 0)
 	{
-		bits[j] = 0;
+		bits[i] = 0;
 	}
 	else
 	{
-		while (1)
+		while (n > 0)
 		{
-			num = calc_power(2, (unsigned long) i);
-			if (num == n)
-				break;
-			if (num > n)
-			{
-				i = i - 1;
-				break;
-			}
+			bits[i] = n % 2 + '0';
+			n /= 2;
 			i++;
 		}
-		while (i >= 0)
-		{
-			if (calc_power(2, (unsigned long) i) <= n)
-			{
-				bits[j] = 1;
-				n -= calc_power(2, (unsigned long) i);
-			}
-			else
-				bits[j] = 0;
-			i--;
-			j++;
-		}
+		bits[i] = '\0';
 	}
-	if (index > j)
+	if (index > i - 1)
 		return (-1);
-	return (bits[index]);
-}
-
-/**
- * calc_power - Computes power problems
- * @base: Base number
- * @power: Index/power
- *
- * Return: Computed value
- */
-unsigned long calc_power(unsigned long base, unsigned long power)
-{
-	if (power == 0)
-		return (1);
-	else
-		return (base * calc_power(base, power - 1));
+	return (bits[index] - '0');
 }
