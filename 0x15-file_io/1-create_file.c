d1e18653154de60457dc2f6adebd922c2f1a500a
Peter Omollo <omollpeter@gmail.com>
2023-11-27 22:23:01 +0300

    Fix 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,21 +11,31 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t count;
+	FILE *fp;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fp = fopen(filename, "r");
+	if (!fp)
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	else
+		fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+	{
+		fclose(fp);
+		close(fd);
+		return (1);
+	}
 
 	count = write(fd, text_content, strlen(text_content) + 1);
 	if (count == -1)
 		return (-1);
 
+	fclose(fp);
 	close(fd);
 	return (1);
 }
