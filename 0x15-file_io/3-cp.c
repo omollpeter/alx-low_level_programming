8645a7a6be111298d9563f56eaa01470e7c58a47
Peter Omollo <omollpeter@gmail.com>
2023-11-28 01:21:02 +0300

    Create a program that copies file content from a file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * main - Checks code
+ * @argc: Number of program parameters
+ * @argv: Array of program paramters
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, cfd1, cfd2, result;
+	FILE *fp;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fp = fopen(argv[2], "r");
+	if (!fp)
+		fd2 = open(argv[2], O_WRONLY | O_CREAT, 0664);
+	else
+		fd2 = open(argv[2], O_WRONLY | O_TRUNC);
+
+	result = copy_file(fd1, fd2);
+	if (result == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	fclose(fp);
+	cfd1 = close(fd1);
+	if (cfd1 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	cfd2 = close(fd2);
+	if (cfd2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
+
+/**
+ * copy_file - Copies content of a file to another file
+ * @fd1: reference to file to copy from
+ * @fd2: reference to file to copy to
+ *
+ * Return: 1 on succcess, -1 othersise
+ */
+int copy_file(int fd1, int fd2)
+{
+	char buffer[1024];
+	ssize_t nb_read = -1, nb_written;
+
+	while (nb_read != 0)
+	{
+		nb_read = read(fd1, buffer, 1024);
+		if (nb_read == -1)
+			return (-1);
+		nb_written = write(fd2, buffer, nb_read);
+		if (nb_written == -1)
+			return (-1);
+	}
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,5 +15,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int copy_file(int fd1, int fd2);
 
 #endif /* MAIN_H */
