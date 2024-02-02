efeedfd826da8f5aa7fad7262a284b95da100694
Peter Omollo <omollpeter@gmail.com>
2024-02-02 05:13:55 +0300

    Fix 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,8 +16,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || strlen(key) == 0)
 		return (0);
-	if (value == NULL)
-		return (0);
 
 	idx = key_index((unsigned char *) key, ht_size);
 	hn = malloc(sizeof(hash_node_t));
